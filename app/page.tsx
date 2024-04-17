01481421500651bf9226e10dc90addc0fdc8ecf8
SunnyBiolie <nguyenthanhdanh1789@gmail.com>
2024-04-17 08:19:32 +0700

    First commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,255 @@
-import Image from "next/image";
+import {
+  IP_Index,
+  PC1_Index,
+  PC2_Index,
+  P_Index,
+  S1DecimalSelector,
+  S2DecimalSelector,
+  S3DecimalSelector,
+  S4DecimalSelector,
+  S5DecimalSelector,
+  S6DecimalSelector,
+  S7DecimalSelector,
+  S8DecimalSelector,
+  eBitSelector_Index,
+  numberOfLeftShifts,
+} from "@/cipher/constants";
 
 export default function Home() {
+  const K = "133457799BBCDFF1";
+
+  const hexToBin = (hexText: string) => {
+    let result: string = "";
+    hexText.split("").forEach((char) => {
+      result += parseInt(char, 16).toString(2).padStart(4, "0");
+    });
+    return result;
+  };
+
+  const binToHex = (binText: string) => {
+    const array = splitIntoNCharArray(binText, 4);
+
+    let result: string = "";
+    array.forEach((item) => {
+      result += parseInt(item, 2).toString(16).toUpperCase();
+    });
+    return result;
+  };
+
+  const splitIntoNCharArray = (text: string, itemLength: number) => {
+    const array: string[] = [];
+    let index: number = 0;
+    let count: number = 1;
+    text.split("").forEach((char) => {
+      if (count > itemLength) {
+        index++;
+        count = 1;
+      }
+
+      if (count === 1) {
+        array[index] = char;
+      } else array[index] += char;
+
+      count++;
+    });
+
+    return array;
+  };
+
+  const getPC1 = (KBinary: string) => {
+    let PC1: string = "";
+
+    PC1_Index.forEach((row) => {
+      row.forEach((indexPlus) => {
+        PC1 += KBinary[indexPlus - 1];
+      });
+    });
+
+    return PC1;
+  };
+
+  /**
+   * Dịch vòng trái một chuỗi 28 ký tự nhị phân theo bảng dịch vòng cho trước (16 lần dịch vòng),
+   * kết quả của vòng trước là bắt đầu của vòng sau.
+   *
+   * Kết quả trả về là một mảng gồm 16 phần tử là kết quả của những lần dịch vòng.
+   *
+   * @param {String} binText
+   */
+  const leftShift = (binText: string) => {
+    const array: string[] = [];
+    let init = binText;
+    numberOfLeftShifts.forEach((value) => {
+      const result = init.substring(value) + init.slice(0, value);
+      array.push(result);
+      init = result;
+    });
+
+    return array;
+  };
+
+  const getPC2 = (CDBinary: string) => {
+    let PC2: string = "";
+
+    PC2_Index.forEach((row) => {
+      row.forEach((indexPlus) => {
+        PC2 += CDBinary[indexPlus - 1];
+      });
+    });
+
+    return PC2;
+  };
+
+  const res = hexToBin(K);
+
+  const pc1 = getPC1(res);
+  const C0 = pc1.slice(0, 28);
+  const D0 = pc1.slice(28);
+
+  const pc1_in_hex = binToHex(pc1);
+
+  const array_Cn = leftShift(C0);
+  const array_Dn = leftShift(D0);
+
+  const array_Kn: string[] = [];
+  for (let i = 0; i < array_Cn.length; i++) {
+    array_Kn.push(getPC2(array_Cn[i] + array_Dn[i]));
+  }
+
+  //
+
+  const M = "0123456789ABCDEF";
+
+  const getIP = (MessageBinary: string) => {
+    let IP: string = "";
+
+    IP_Index.forEach((row) => {
+      row.forEach((indexPlus) => {
+        IP += MessageBinary[indexPlus - 1];
+      });
+    });
+
+    return IP;
+  };
+
+  const getExpandHalfRight = (RnBinary: string) => {
+    let E_Rn: string = "";
+
+    eBitSelector_Index.forEach((row) => {
+      row.forEach((indexPlus) => {
+        E_Rn += RnBinary[indexPlus - 1];
+      });
+    });
+
+    return E_Rn;
+  };
+
+  const XOR_Binary = (KnBinary: string, E_RnBinary: string) => {
+    let result = "";
+    for (let i = 0; i < KnBinary.length; i++) {
+      if (KnBinary[i] === E_RnBinary[i]) {
+        result += "0";
+      } else result += "1";
+    }
+
+    return result;
+  };
+
+  const getSBoxn = (sixBitText: string, indexPlus: number) => {
+    const selectRow = sixBitText[0] + sixBitText[5];
+    let x: number = 0;
+    if (selectRow === "00") x = 0;
+    else if (selectRow === "01") x = 1;
+    else if (selectRow === "10") x = 2;
+    else if (selectRow === "11") x = 3;
+
+    const selectColumn = sixBitText.slice(1, 5);
+    const y = parseInt(selectColumn, 2);
+
+    let decimalResult: number;
+    switch (indexPlus) {
+      case 1:
+        decimalResult = S1DecimalSelector[x][y];
+        break;
+      case 2:
+        decimalResult = S2DecimalSelector[x][y];
+        break;
+      case 3:
+        decimalResult = S3DecimalSelector[x][y];
+        break;
+      case 4:
+        decimalResult = S4DecimalSelector[x][y];
+        break;
+      case 5:
+        decimalResult = S5DecimalSelector[x][y];
+        break;
+      case 6:
+        decimalResult = S6DecimalSelector[x][y];
+        break;
+      case 7:
+        decimalResult = S7DecimalSelector[x][y];
+        break;
+      case 8:
+        decimalResult = S8DecimalSelector[x][y];
+        break;
+    }
+
+    return decimalResult!.toString(2).padStart(4, "0");
+  };
+
+  const permutationP = (sBox_Binary: string) => {
+    let P: string = "";
+
+    P_Index.forEach((row) => {
+      row.forEach((indexPlus) => {
+        P += sBox_Binary[indexPlus - 1];
+      });
+    });
+
+    return P;
+  };
+
+  // const display = (text: string, type: "bin" | "hex") => {
+  //   switch (type) {
+  //     case "bin":
+  //       break;
+  //   }
+  // };
+
+  const message = hexToBin(M);
+  const IP = getIP(message);
+
+  const L0 = IP.slice(0, 32);
+  const R0 = IP.slice(32);
+  const E_R0 = getExpandHalfRight(R0);
+
+  const B = XOR_Binary(array_Kn[0], E_R0);
+
+  const array_B = splitIntoNCharArray(B, 6);
+
+  let sBox_Binary: string = "";
+  array_B.forEach((sixBit, index) => {
+    sBox_Binary += getSBoxn(sixBit, index + 1);
+  });
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="flex flex-col items-center justify-center gap-y-8">
+      <div>
+        {array_Kn.map((ele, index) => (
+          <p key={index}>{binToHex(ele)}</p>
+        ))}
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div>
+        {array_B.map((ele, index) => (
+          <p key={index}>{ele}</p>
+        ))}
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div>
+        {array_B.map((ele, index) => (
+          <p key={index}>{binToHex(getSBoxn(ele, index + 1))}</p>
+        ))}
       </div>
-    </main>
+      <div>{binToHex(permutationP(sBox_Binary))}</div>
+    </div>
   );
 }
---
+++ cipher/feistel.ts
@@ -0,0 +1,10 @@
+/**
+ *
+ * @param {String} Rn_Binary n start from 0
+ * @param {String} Knpp_Binary n + 1
+ *
+ * @example feistel(R0, K1) {}
+ */
+export default function feistel(Rn_Binary: string, Knpp_Binary: string) {
+  // Mở rộng nửa phải
+}
